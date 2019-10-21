61ea6db9bae68eebba006f48bd2fd750ecabf942
李明叡 <leeray1208@limingruide-MacBook-Pro.local>
2019-10-21 16:05:22 -0700

    add Taiwan

---
+++ driving_again.py
@@ -0,0 +1,9 @@
+# let user input age and country, output is whether the user can drive or not
+country = input('Which country are you from: ')
+age = input('Please enter your age:')
+age = int(age)	# casting very important
+if country == 'Taiwan':
+	if age >= 18:
+		print('You can take driving licence test')
+	else:
+		print('You can not take driving licence test yet')
